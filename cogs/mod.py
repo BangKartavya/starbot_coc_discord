35bb004b6ff18053380383611d15e90a737bfa2a
Kartavya Bang <bangkartavya@gmail.com>
2024-06-30 00:46:57 +0530

    some secret

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
-import discord
-
+import discord,dotenv,os
+from dynamic_token_gen.main import get_keys,login,load,logout
 class Moderation(commands.Cog):
     def __init__(self,client:commands.Bot):
         self.client: commands.Bot = client
@@ -19,7 +19,23 @@ class Moderation(commands.Cog):
         await ctx.send("Restarting...")
         raise KeyboardInterrupt()
 
-
+    @commands.command()
+    @commands.is_owner()
+    async def keys(self,ctx:commands.Context):
+        dotenv_file = dotenv.find_dotenv()
+        dotenv.load_dotenv(dotenv_file)
+        email = os.getenv('email')
+        password = os.getenv('password')
+        
+        log = login(email,password)
+        keys = get_keys(log)
+        
+        karnav = await self.client.fetch_user(ctx.message.author.id)
+        
+        for i in keys.json()['keys']:
+            await karnav.send(f"Key - {i['key']}")
+        
+        logout(log)
 
 def setup(client:commands.Bot):
     client.add_cog(Moderation(client))
