68c4937af32f2bdc38d2120182ab894481a89ae5
Kartavya Bang <bangkartavya@gmail.com>
2024-07-02 13:38:05 +0530

    will take care of it later

--- keep_alive.py
+++ keep_alive.py
@@ -1,21 +1,21 @@
-# from flask import Flask,render_template
-# from threading import Thread
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return "Alive"
-
-# def run():
-#     app.run(host='0.0.0.0',port=8080)
-
-# def keep_alive():
-#     t = Thread(target=run)
-#     t.start()
-
-from dashboard.dashboard import app
+from flask import Flask,render_template
 from threading import Thread
 
-def keep_alive():   
-    app.run(host = '0.0.0.0',port=5000)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Alive"
+
+def run():
+    app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# from dashboard.dashboard import app
+# from threading import Thread
+
+# def keep_alive():   
+#     app.run(host = '0.0.0.0',port=5000)
