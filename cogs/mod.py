7b211172e618bfc66800c920e9859d77520b4589
Kartavya Bang <bangkartavya@gmail.com>
2024-07-11 19:33:39 +0530

    get api key on the fly

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 import discord,dotenv,os
-from dynamic_token_gen.main import get_keys,login,load,logout
+from dynamic_token_gen.main import *
 
 class Moderation(commands.Cog):
     def __init__(self,client:commands.Bot):
@@ -21,7 +21,7 @@ class Moderation(commands.Cog):
         await ctx.send("Restarting...")
         raise KeyboardInterrupt()
 
-    @commands.command(aliases = ["key"],description = "ONYL FOR OWNER... get the api keys for a session")
+    @commands.command(aliases = ["key"],description = "ONLY FOR OWNER... get the api keys for a session")
     @commands.is_owner()
     async def keys(self,ctx:commands.Context):
         dotenv_file = dotenv.find_dotenv()
@@ -39,6 +39,29 @@ class Moderation(commands.Cog):
             await karnav.send(f"Key - {i['key']}")
         
         logout(log)
+    @commands.command(aliases = ["gk"],description = "ONLY FOW OWNER... generate api_key")
+    @commands.is_owner()
+    async def generate_key(self,ctx:commands.Context):
+        dotenv_file = dotenv.find_dotenv()
+        dotenv.load_dotenv(dotenv_file)
+        email = os.getenv('email')
+        password = os.getenv('password')
+
+        log = login(email,password)
+        ip = load(log).json()['developer']['prevLoginIp']
+
+        token = get_key_with_ip(log,ip)
+
+        if not token:
+            remove_keys(log)
+            token = create_key(log,'key','key',ip)
+
+        logout(log)
+
+
+        os.environ['COC_API_KEY'] = token
+
+        dotenv.set_key(dotenv_file,'COC_API_KEY',os.environ['COC_API_KEY'])
 
 def setup(client:commands.Bot):
     client.add_cog(Moderation(client))
