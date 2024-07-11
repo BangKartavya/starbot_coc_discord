fb8b91aed25e4ae9a216e0371bf9d54287f936a9
Kartavya Bang <bangkartavya@gmail.com>
2024-07-11 19:02:20 +0530

    again

--- keep_alive_test.py
+++ keep_alive_test.py
@@ -1,7 +1,8 @@
 from dashboard.dashboard import app
 from mangum import Mangum
 import os
+import uvicorn
 
 def run():
     os.environ['AUTHLIB_INSECURE_TRANSPORT'] = '1'
-    handler = Mangum(app)
+    uvicorn.run(app,host='0.0.0.0',port=8080)
