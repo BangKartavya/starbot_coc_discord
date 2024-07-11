fb8d858398c69b4649bcceadc762cee22397be7f
Kartavya Bang <bangkartavya@gmail.com>
2024-07-11 19:11:05 +0530

    hopefully final attempt

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import dotenv
 import os
 from cogs.help import MyHelp
 from dynamic_token_gen.main import *
-from keep_alive_test import run
+from keep_alive_test import keep_alive
 
-run()
+keep_alive()
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv()
 
